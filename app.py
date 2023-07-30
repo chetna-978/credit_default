69336ddd5ddddcdd3733cbb31e866b64ee8bb690
chetna-978 <happychetna5@gmail.com>
2023-07-30 16:59:38 +0530

    updated

--- app.py
+++ app.py
@@ -3,58 +3,90 @@ import pandas as pd
 from credit_default.logger import logging
 from credit_default.exception import CustomException
 from credit_default.utils import load_object
-from credit_default.pipeline.prediction_pipeline import BatchPredictionPipeline
+from credit_default.predictor import ModelResolver
 from datetime import datetime
+from credit_default.components.data_transformation import DataTransformation
 import os
-import sys
 
 app = Flask(__name__)
 
-# Create an instance of BatchPredictionPipeline
-predict_pipeline = BatchPredictionPipeline()
+PREDICTION_DIR = "prediction"
+
+def apply_transformations(df, data_transformation, robust_scaler):
+    transformed_df = data_transformation.data_modified(df)
+    transformed_df = robust_scaler.transform(transformed_df)
+    return transformed_df
+
+def start_batch_prediction(input_file_path):
+    try:
+        os.makedirs(PREDICTION_DIR, exist_ok=True)
+        logging.info("Creating model resolver object")
+        model_resolver = ModelResolver(model_registry="saved_models")
+        logging.info(f"Reading file: {input_file_path}")
+        df = pd.read_csv(input_file_path)
+
+        logging.info("Loading data transformation object")
+        transformation_path = model_resolver.get_latest_transformer_path()
+        data_transformation = DataTransformation.load_object(file_path=transformation_path)
+
+        logging.info("Loading robust scaler object")
+        robust_scaler = load_object(file_path=data_transformation.data_transformation_config.transform_object_path)
+
+        logging.info("Applying transformations")
+        transformed_df = apply_transformations(df, data_transformation, robust_scaler)
+
+        logging.info("Loading model to make prediction")
+        model = load_object(file_path=model_resolver.get_latest_model_path())
+
+        prediction = model.predict(transformed_df)
+
+        df["prediction"] = prediction
+
+        prediction_file_name = os.path.basename(input_file_path).replace(".csv", f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.csv")
+        prediction_file_path = os.path.join(PREDICTION_DIR, prediction_file_name)
+        df.to_csv(prediction_file_path, index=False, header=True)
+
+        return prediction_file_path
+    except Exception as e:
+        raise CustomException(e, sys)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'GET':
         return render_template('index.html')
-    else:
-        data = {
-            'limit_bal': float(request.form.get('LIMIT_BAL')),
-            'sex': int(request.form.get('SEX')),
-            'education': int(request.form.get('EDUCATION')),
-            'marriage': int(request.form.get('MARRIAGE')),
-            'age': int(request.form.get('AGE')),
-            'pay_0': int(request.form.get('PAY_0')),
-            'pay_2': int(request.form.get('PAY_2')),
-            'pay_3': int(request.form.get('PAY_3')),
-            'pay_4': int(request.form.get('PAY_4')),
-            'pay_5': int(request.form.get('PAY_5')),
-            'pay_6': int(request.form.get('PAY_6')),
-            'BILL_AMT1': int(request.form.get('BILL_AMT1')),
-            'BILL_AMT2': int(request.form.get('BILL_AMT2')),
-            'BILL_AMT3': int(request.form.get('BILL_AMT3')),
-            'BILL_AMT4': int(request.form.get('BILL_AMT4')),
-            'BILL_AMT5': int(request.form.get('BILL_AMT5')),
-            'BILL_AMT6': int(request.form.get('BILL_AMT6')),
-            'PAY_AMT1': int(request.form.get('PAY_AMT1')),
-            'PAY_AMT2': int(request.form.get('PAY_AMT2')),
-            'PAY_AMT3': int(request.form.get('PAY_AMT3')),
-            'PAY_AMT4': int(request.form.get('PAY_AMT4')),
-            'PAY_AMT5': int(request.form.get('PAY_AMT5')),
-            'PAY_AMT6': int(request.form.get('PAY_AMT6')),
-        }
-
+    elif request.method == 'POST':
         try:
-            prediction = predict_pipeline.predict(data)
-            # Format the prediction result to display
-            result = "Default" if prediction[0] == 1 else "Not Default"
-            return render_template('index.html', results=result)
+            file = request.files['file']
+            file_path = f"uploads/{file.filename}"
+            file.save(file_path)
+
+            data = pd.read_csv(file_path)
+
+            prediction = start_batch_prediction(data)
+
+            return render_template('index.html', prediction=round(prediction, 2))
         except Exception as e:
-            return render_template('index.html', error_message=str(e))
+            return jsonify({'error': str(e)})
+
+@app.route('/predictAPI', methods=['POST'])
+def predict_api():
+    if request.method == 'POST':
+        try:
+            file = request.files['file']
+            file_path = f"uploads/{file.filename}"
+            file.save(file_path)
+
+            data = pd.read_csv(file_path)
+
+            prediction = start_batch_prediction(data)
+
+            return jsonify({'prediction': round(prediction, 2)})
+        except Exception as e:
+            return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', '8080')
@@ -64,3 +96,4 @@ if __name__ == '__main__':
         port = int(port)
     logging.info(f"Flask application running on port {port}")
     app.run(host="0.0.0.0", port=port, debug=False)
+    
